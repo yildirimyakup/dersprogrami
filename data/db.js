2c53fd618b7f0a31e009d7615fa980118b7ad8c9
Yakup YILDIRIM <yakup.yildirim.y@gmail.com>
2024-08-25 06:05:13 -0400

    sequelize ("Bağlantı")

---
+++ data/db.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2");
+const config = require("../config");
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
+    dialect: "mysql",
+    host: config.db.host
+});
+
+async function connect() {
+    try {
+        await sequelize.authenticate();
+        console.log("mysql server bağlantısı yapıldı");
+    }
+    catch(err) {
+        console.log("bağlantı hatası ", err);
+    }
+}
+
+connect();
+
+module.exports = sequelize;
