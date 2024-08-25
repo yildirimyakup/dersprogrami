7128f3b308ef8c0906e510f9d0460fa0333250af
Yakup YILDIRIM <yakup.yildirim.y@gmail.com>
2024-08-25 10:22:33 -0400

    login page and login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
-const User = require("../models/user");
+const { where } = require("sequelize");
+const User = require("../models/user"); // Kullanıcı modelini içe aktarır
+const bcrypt = require("bcrypt"); // Şifreleri hash'lemek için bcrypt kütüphanesini içe aktarır
 
 // GET /register: Kullanıcı kayıt sayfasını render eden işlev
 exports.get_register = async (req, res) => {
@@ -10,6 +12,7 @@ exports.get_register = async (req, res) => {
   } catch (error) {
     // Hata oluşursa, hata mesajını konsola yazdırır
     console.log("Sayfa => auth.js:", error);
+    res.redirect("/account/auth/login");
   }
 };
 
@@ -19,17 +22,77 @@ exports.post_register = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
+    // Şifreyi bcrypt ile hash'ler (şifreleme işlemi)
+    const hashPassword = await bcrypt.hash(password, 10);
+
     // Yeni kullanıcıyı veritabanına kaydeder
     await User.create({
-      full_name: name,
-      e_mail: email,
-      password: password,
+      full_name: name, // Kullanıcının tam adı
+      e_mail: email, // Kullanıcının e-posta adresi
+      password: hashPassword, // Hashlenmiş şifre
     });
+
     // Kayıt başarılıysa, kullanıcıyı login sayfasına yönlendirir
-    res.redirect("account/login");
+    res.redirect("/account/auth/login");
   } catch (error) {
     // Hata oluşursa, hata mesajını konsola yazdırır ve kullanıcıyı kayıt sayfasına yönlendirir
     console.log("Sayfa => auth.js:", error);
-    res.redirect("/account/register?error=true");
+    res.redirect("/account/auth/register?error=true"); // Hata parametresi ile kayıt sayfasına yönlendirir
+  }
+};
+
+// GET /login: Kullanıcı giriş sayfasını render eden işlev
+exports.get_login = async (req, res) => {
+  try {
+    // 'auth/login' şablonunu 'title' değişkeni ile render eder
+    res.render("auth/login", {
+      title: "Login Page.",
+    });
+  } catch (error) {
+    // Hata oluşursa, hata mesajını konsola yazdırır
+    console.log("Sayfa => login.js:", error);
+    res.redirect("/account/login");
+  }
+};
+
+// POST /login: Kullanıcı giriş formundan gelen verileri işleyen işlev
+exports.post_login = async (req, res) => {
+  // Formdan gelen kullanıcı verilerini alır
+  const { email, password } = req.body;
+
+  try {
+    // Şifreyi bcrypt ile hash'ler (şifreleme işlemi)
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const userControl = await User.findOne({
+      where: {
+        e_mail: email,
+      },
+    });
+
+    if (!userControl) {
+      return res.render("auth/login", {
+        title: "Login Page",
+        message: "E-mail hatalı",
+      });
+    }
+
+    const match = await bcrypt.compare(password, userControl.password);
+
+    if (match) {
+      // Login Yapıldı...
+      console.log("giriş Yapıldı..");
+      return;
+    }
+
+    // Giriş başarılıysa, kullanıcıyı login sayfasına yönlendirir
+    res.render("auth/login", {
+      title: "Login Page",
+      message: "Parola Hatalı",
+    });
+  } catch (error) {
+    // Hata oluşursa, hata mesajını konsola yazdırır ve kullanıcıyı kayıt sayfasına yönlendirir
+    console.log("Sayfa => login.js:", error);
+    res.redirect("/auth/login?error=true"); // Hata parametresi ile kayıt sayfasına yönlendirir
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -9,4 +9,10 @@ router.get("/auth/register", authController.get_register);
 // POST /auth/register: Kullanıcı kayıt formundan gelen verileri işleyen işlevi tanımlar
 router.post("/auth/register", authController.post_register);
 
+// GET /auth/login: Kullanıcı giriş sayfasını sunan işlevi tanımlar
+router.get("/auth/login", authController.get_login);
+
+// POST /auth/login: Kullanıcı giriş formundan gelen verileri işleyen işlevi tanımlar
+router.post("/auth/login", authController.post_login);
+
 module.exports = router; // Router'ı dışa aktarır, böylece diğer modüllerde kullanılabilir
